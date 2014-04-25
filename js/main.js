b01cc56606488d26bc1b25f7d06df1de2441bc1a
bookworm-phonegap <admin@bookworm.ru>
2014-04-25 12:58:17 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 var app = {
+    
+
+showAlert: function (message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+},
+
+
 
     findByName: function() {
         console.log('findByName');
@@ -11,12 +22,16 @@ var app = {
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
         });
-    },
+},
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+initialize: function() {
+    alert('123');
+    var self = this;
+    this.store = new MemoryStore(function() {
+        self.showAlert('Store Initialized', 'Info');
+    });
+    $('.search-key').on('keyup', $.proxy(this.findByName, this));
+}
 
 };
 
